27f8f9e2a2df64303d1573fdb346778a9b69b9ce
NickVanGerwen <47970324+NickVanGerwen@users.noreply.github.com>
2021-11-15 11:42:18 +0100

    EFC werkt

--- back-end/Plant-Project-Backend/DBContext/PlantDBContext.cs
+++ back-end/Plant-Project-Backend/DBContext/PlantDBContext.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Logic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -9,7 +12,14 @@ namespace DBContext
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Group> Groups { get; set; }
-
         public PlantDBContext(DbContextOptions options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder) => base.OnModelCreating(builder);
+
+        public List<Group> GetGroups()
+        {
+            Groups.Add(new Group() { Name = "group 4", Password = "123"});
+            SaveChanges();        
+                return Groups.ToList();
+        }
     }
 }
--- back-end/Plant-Project-Backend/Plant-Project-Backend/Controllers/GroupController.cs
+++ back-end/Plant-Project-Backend/Plant-Project-Backend/Controllers/GroupController.cs
@@ -14,30 +14,29 @@ namespace Plant_Project_Backend.Controllers
     public class GroupController : ControllerBase
     {
         User user;
-        public GroupController()
+        PlantDBContext context;
+        public GroupController(PlantDBContext plantDBContext)
         {
+            context = plantDBContext;
             user = new Logic.User();
         }
 
         [HttpGet]
         public List<Group> GetGroups(int userId)
         {
-            PlantDBContext context = new PlantDBContext(new Microsoft.EntityFrameworkCore.DbContextOptions<PlantDBContext>());
-            var groups = new List<Group>();
-            groups.Add(new Group() { Name = "group 1", Password = "123", Id = 1 });
-            groups.Add(new Group() { Name = "group 2", Password = "123", Id = 2 });
-            groups.Add(new Group() { Name = "group 3", Password = "123", Id = 3 });
-            groups.Add(new Group() { Name = "group 4", Password = "123", Id = 27 });
-            try
-            {
-                context.AddRange(groups);
-                context.SaveChanges();
-                return groups;
-            }
-            catch
-            {
-            return new List<Group>();
-            }
+            //var groups = new List<Group>();
+            //groups.Add(new Group() { Name = "group 1", Password = "123", Id = 1 });
+            //groups.Add(new Group() { Name = "group 2", Password = "123", Id = 2 });
+            //groups.Add(new Group() { Name = "group 3", Password = "123", Id = 3 });
+            //groups.Add(new Group() { Name = "group 4", Password = "123", Id = 27 });
+
+            ////Group group = new Group() { Name = "group 4", Password = "123", Id = 27 };
+
+            //context.Groups.AddRange(groups);
+            //context.SaveChanges();
+
+            return context.GetGroups();
+
         }
 
         [HttpPost]
